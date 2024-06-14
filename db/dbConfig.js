28b42e8022c37fb1ef99b40bddfffa9199775ac8
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-14 09:39:26 -0400

    updated the DB schema and the initial values inserted into the articles and summaries tables, and updated the DB config file

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,16 +1,14 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const url = process.env.DATABASE_URL;
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+  };
 
-// const cn = {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//   };
-
-const db = pgp(url);
+const db = pgp(cn);
 
 module.exports = db;
--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,9 @@
 -- db/seed.sql
 \c news
 
-INSERT INTO articles (title, summary, created_at, updated_at)
+INSERT INTO articles (title, abstract, created_at, updated_at)
 VALUES 
-('some gigantic title', 'summary text. more text, and even more over here', NOW(), NOW());
+('some gigantic title', 'a very small abstract', NOW(), NOW());
 
 INSERT INTO summaries (summary, article_id, created_at, updated_at)
 VALUES
