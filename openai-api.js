accb5e3db793ba8698e0f031d1fc3b9d967cd35f
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-15 21:52:49 -0400

    switch on the summaryType argument to get the grade level for the summary, and update the api request to incorporate the corresponding grade level

--- openai-api.js
+++ openai-api.js
@@ -5,11 +5,28 @@ const OpenAI = require("openai");
 
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
-async function main(abstract) {
+async function main(abstract, summaryType = '8') {
+  let grade;
+  switch (summaryType) {
+    case '12':
+      grade = 12;
+      break;
+
+    case '13':
+      grade = NaN;
+      break;
+    
+    default:
+      grade = 8;
+      break;
+  }
+
+  const userMessage = `Based on the abstract, ${abstract}, find the related news article and summarize it - maximum 300 words and understandable for an ${typeof grade === 'number' ? grade + 'th grader ' + 'and below' : 'adult'}`
+
   const completion = await openai.chat.completions.create({
     messages: [
         { role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: `Based on the abstract, ${abstract}, find the related news article and summarize it - maximum 300 words and understandable for an 8th grader and below` }
+        { role: "user", content: userMessage }
     ],
     model: "gpt-3.5-turbo",
   });
