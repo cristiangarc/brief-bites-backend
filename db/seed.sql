4f8ca32d43f5c87d6c7ea2eed2420cf3589d8177
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-15 22:05:33 -0400

    updated the db schema and the initial values inserted into the articles table; deleted the summaries table

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,6 @@
 -- db/seed.sql
 \c news
 
-INSERT INTO articles (title, abstract, created_at, updated_at)
+INSERT INTO articles (title, abstract, summary_type, created_at, updated_at)
 VALUES 
-('some gigantic title', 'a very small abstract', NOW(), NOW());
-
-INSERT INTO summaries (summary, article_id, created_at, updated_at)
-VALUES
-('summary', 1, NOW(), NOW());
+('some gigantic title', 'a very small abstract', 8, NOW(), NOW());
